4c7905b55457af3023132fbb77ee6b7ff876353f
paucespedes <paucespedes.code@gmail.com>
2024-02-06 16:48:53 +0100

    save noisy images

--- data/dataset_video_test.py
+++ data/dataset_video_test.py
@@ -128,8 +128,9 @@ class VideoRecurrentTestDataset(data.Dataset):
 
             for x in range(t):
                 # Lack of squeeze later on with some data??
+                #cpu().detach().numpy()
                 img = imgs_lq[x]
-                img = img.data.squeeze().float().cpu().clamp_(0, 1).numpy()
+                img = img.data.squeeze().float().cpu().detach().clamp_(0, 1).numpy()
                 img = np.transpose(img[[2, 1, 0], :, :], (1, 2, 0))  # CHW-RGB to HCW-BGR
                 img = (img * 255.0).round().astype(np.uint8)  # float32 to uint8
                 cv2.imwrite(f'results/noisy-imgs/{index}/{x}.png', img)
