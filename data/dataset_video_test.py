30028b154427c963b1cec32b664ce78cec4ccf7c
paucespedes <paucespedes.code@gmail.com>
2024-02-08 15:27:48 +0100

    rgb permutation

--- data/dataset_video_test.py
+++ data/dataset_video_test.py
@@ -124,7 +124,7 @@ class VideoRecurrentTestDataset(data.Dataset):
             noise_level = torch.ones((1, 1, 1, 1)) * self.sigma
             noise = torch.normal(mean=0, std=noise_level.expand_as(imgs_gt))
             imgs_lq = imgs_gt + noise
-            # imgs_lq.clamp_(0, 1)
+            imgs_lq.clamp_(0, 1)
             # print('Images LQ:')
             # print(imgs_lq)
             t, _, h, w = imgs_gt.shape
@@ -134,7 +134,7 @@ class VideoRecurrentTestDataset(data.Dataset):
                 #cpu().detach().numpy()
                 img = imgs_lq[x].clone().detach()
                 img = img.data.squeeze().float().cpu().clamp_(0, 1).numpy()
-                img = np.transpose(img[[2, 1, 0], :, :], (1, 2, 0))  # CHW-RGB to HCW-BGR
+                img = img[:, :, [2, 1, 0]]  # CHW-RGB to HCW-BGR
                 img = (img * 255.0).round().astype(np.uint8)  # float32 to uint8
                 cv2.imwrite(f'results/noisy-imgs/{index}/{x}.png', img)
             imgs_lq = torch.cat([imgs_lq, noise_level.expand(t, 1, h, w)], 1)
